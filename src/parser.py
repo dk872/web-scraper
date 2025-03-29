f9bcae5eb9e12b23066d3b367cfe32639b23d0d2
dk872 <dimakulyk2005@gmail.com>
2025-03-29 21:59:29 +0200

    Add function to extract company details
    Implemented parse_company_info to extract key details about a company,
    such as its name, rating, category, and contact information. The function
    uses CSS selectors to locate the relevant elements in the HTML.

--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def get_page_soup(url, default=None):
     try:
         response = requests.get(url, timeout=10)
@@ -10,4 +11,22 @@ def get_page_soup(url, default=None):
     except requests.exceptions.RequestException as e:
         print(f"Error while loading {url}: {e}")
         return default
-    
+
+
+def parse_company_info(soup):
+    def get_text_or_default(selector, default='Unknown'):
+        element = soup.select_one(selector)
+        return element.get_text(strip=True) if element else default
+
+    name = get_text_or_default(
+        'span.typography_display-s__pKPhT.typography_appearance-default__t8iAq.title_displayName__OBSVU')
+    rating = get_text_or_default(
+        'p.typography_body-l__v5JLj.typography_appearance-subtle__PYOVM')
+    category = get_text_or_default(
+        'a.link_internal__Eam_b.typography_appearance-action__u_Du4.link_link__jBdLV.link_notUnderlined__y4Qsc.'
+        'styles_breadcrumbLink__BTE_W')
+    contact = get_text_or_default(
+        'a.link_internal__Eam_b.typography_body-l__v5JLj.typography_appearance-action__u_Du4.link_link__jBdLV.'
+        'link_notUnderlined__y4Qsc.styles_underline__j1_xR')
+
+    return name, rating, category, contact
