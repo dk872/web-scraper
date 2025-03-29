a7ef5c3e845d0c0175333ae58d7ac56d8c3bea8c
dk872 <dimakulyk2005@gmail.com>
2025-03-29 22:04:57 +0200

    Add function to prompt user for URLs
    Implemented a function that prompts the user to enter up to 5 URLs.
    Users can stop entering URLs early by typing 'no'. This function ensures that
    the program handles user input correctly and does not exceed the maximum
    allowed number of URLs.

--- src/main.py
+++ src/main.py
@@ -1,2 +1,19 @@
 from parser import parse_data
 
+
+def prompt_user_for_urls():
+    urls = []
+    max_links = 5
+    print(f"Welcome to the Web-Scraper! You can provide up to {max_links} links. \nEnter 'no' to stop.")
+
+    while len(urls) < max_links:
+        remaining_links = max_links - len(urls)
+        url = input(f"Enter the link (you can enter {remaining_links} more link(s)): ")
+
+        if url.lower() == 'no':
+            break
+
+        if url:
+            urls.append(url)
+
+    return urls
